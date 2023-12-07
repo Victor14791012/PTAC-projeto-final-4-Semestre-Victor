6f75abafb131937110284ec3dc0ac5fc991b1936
Victor14791012 <victor.carvalhok23@gmail.com>
2023-12-06 23:32:11 -0400

    update
    criando a função salvar

--- src/Registro.jsx
+++ src/Registro.jsx
@@ -16,6 +16,36 @@ export default function Registro() {
   }, [Lista]);
 
   // Função para adicionar uma atividade
+  const salvar = (e) => {
+    e.preventDefault();
+
+    // Extrair a parte após a última barra do link do YouTube
+    const linkYoutubeID = linkYoutube.substring(linkYoutube.lastIndexOf("/") + 1);
+
+    // Adicionar um novo objeto à lista
+    const novoVideo = {
+      nomeVideo: nomeVideo,
+      descricao: descricao,
+      duracao: duracao,
+      tema: tema,
+      linkYoutube: linkYoutubeID, // Usar apenas a parte após a última barra
+      id: id
+    };
+
+    setLista([...Lista, novoVideo]);
+    setId(id + 1); // Incrementar o contador de IDs
+
+    // Limpar os campos do formulário
+    setNomeVideo("");
+    setDescricao("");
+    setDuracao("");
+    setTema("");
+    setLinkYoutube("");
+
+    alert(nomeVideo);
+    console.log(novoVideo);
+    console.log(Lista);
+  };
 
 
 
