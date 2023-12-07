1714c42488a225a266320dbed8925da2e16fdb32
Victor14791012 <victor.carvalhok23@gmail.com>
2023-12-06 23:34:35 -0400

    update
    criando interface do registro no html

--- src/Registro.jsx
+++ src/Registro.jsx
@@ -61,6 +61,87 @@ export default function Registro() {
   };
 
   return (
-   
+    <div>
+      <div className="container" id="transparente">
+        <Link to="/" className="m-2">
+          Home
+        </Link>
+        <Link to="/destaque">Destaque</Link>
+        <div className="bg-dark text-white rounded-2 p-2">
+          <h1>Portal Geek</h1>
+          <h6>Adicione seus vídeos favoritos do YouTube</h6>
+
+          <form onSubmit={salvar}>
+            <div className="mb-3">
+              <label htmlFor="nomeVideo" className="form-label">
+                Nome do Vídeo
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="nomeVideo"
+                value={nomeVideo}
+                onChange={(e) => setNomeVideo(e.target.value)}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="descricao" className="form-label">
+                Descrição
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="descricao"
+                value={descricao}
+                onChange={(e) => setDescricao(e.target.value)}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="duracao" className="form-label">
+                Duração
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="duracao"
+                value={duracao}
+                onChange={(e) => setDuracao(e.target.value)}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="tema" className="form-label">
+                Tema
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="tema"
+                value={tema}
+                onChange={(e) => setTema(e.target.value)}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="linkYoutube" className="form-label">
+                Link do YouTube
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="linkYoutube"
+                value={linkYoutube}
+                onChange={(e) => setLinkYoutube(e.target.value)}
+              />
+            </div>
+            <button className="btn btn-primary">ADD</button>
+            <button className="btn btn-danger mx-2" onClick={deletarTodos}>
+              DEL <i className="bi bi-trash"></i>
+            </button>
+          </form>
+        </div>
+
+        
+      </div>
+    </div>
   );
 }
+
