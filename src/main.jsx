915dfe2155632bbe6d5b7434515c7d722f57cbf0
Victor14791012 <victor.carvalhok23@gmail.com>
2023-12-06 23:11:43 -0400

    6-12
    nesse commit criei os arquivos que serão utilizados no projeto e criei as rotas necessárias

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,21 @@
+// main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './Home'
+import Detalhe from './Detalhe'
+import Destaque from './Destaque'
+import Registro from './Registro'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-     <BrowserRouter>
+    <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />}></Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/detalhe/:id" element={<Detalhe />}></Route>
+        <Route path="/destaque" element={<Destaque />} />
+        <Route path="/registro" element={<Registro />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
