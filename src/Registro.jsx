88e66ff7eb6b4ab1db1a51f47bc48e3c65eaba9a
Victor14791012 <victor.carvalhok23@gmail.com>
2023-12-06 23:31:01 -0400

    update
    Configuração do gerenciamento de estado e armazenamento no localStorage

--- src/Registro.jsx
+++ src/Registro.jsx
@@ -2,6 +2,22 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 export default function Registro() {
+  const listaLocalStorage = JSON.parse(localStorage.getItem("Lista"));
+  const [nomeVideo, setNomeVideo] = useState("");
+  const [descricao, setDescricao] = useState("");
+  const [duracao, setDuracao] = useState("");
+  const [tema, setTema] = useState("");
+  const [linkYoutube, setLinkYoutube] = useState("");
+  const [Lista, setLista] = useState(listaLocalStorage || []);
+  const [id, setId] = useState(1);
+
+  useEffect(() => {
+    localStorage.setItem("Lista", JSON.stringify(Lista));
+  }, [Lista]);
+
+  // Função para adicionar uma atividade
+
+
 
   return (
    
